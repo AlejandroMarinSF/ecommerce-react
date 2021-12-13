31f7c48c98eb1e22bb5faab65ca2cb21d1a1996f
Alejandro <alejandro.marin@iurco.com>
2021-12-13 10:49:02 -0300

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51K5t11AQhpvoxR5m1AN0a2V2p9SHURWffJ7FmtHeC8OfDSgcWqqdi3hQQeoe3Jm5O9OQAMQ06I0IkraHghUZJdLY00trvilIfa'
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful')
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+};
+
+export default StripeCheckoutButton
